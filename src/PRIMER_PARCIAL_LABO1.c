19a9a5e2ce706eeab2264349a1e6cb979b8d803b
ezeematias <eunia69@gmai.com>
2020-10-10 16:56:38 -0300

    Creación de proyecto

---
+++ src/PRIMER_PARCIAL_LABO1.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : PRIMER_PARCIAL_LABO1.c
+ Author      : Unía, Ezequiel Matías
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	return EXIT_SUCCESS;
+}
